d1040cfed3421f63f725fab74b0b48382d597253
badBlackShark <Timdaniel.Engelhardt@web.de>
2019-04-27 15:55:25 +0200

    Changed bot's prefix, since the previous one was taken.

--- src/middlewares/command.cr
+++ src/middlewares/command.cr
@@ -2,7 +2,7 @@ class GodsMatchmaking::Command
   getter name : String
 
   def initialize(name : String)
-    @name = "-" + name.downcase # Prefix is set, seeing that this is a single server bot.
+    @name = "+" + name.downcase # Prefix is set, seeing that this is a single server bot.
   end
 
   def call(payload : Discord::Message, context : Discord::Context)
--- src/plugins/help.cr
+++ src/plugins/help.cr
@@ -19,10 +19,10 @@ class GodsMatchmaking::Help
       value: "Joins you into the queue of people who are currently looking for partners.\n"\
              "Automatically removes you from the queue when you go offline."
     )
-    fields << Discord::EmbedField.new(name: "-leave",   value: "Removes you from the queue.")
-    fields << Discord::EmbedField.new(name: "-queue",   value: "Shows all the people currently looking for partners.")
-    fields << Discord::EmbedField.new(name: "-matches", value: "Shows you everyone in the queue who can potentially play with you.")
-    fields << Discord::EmbedField.new(name: "-info",    value: "Displays some info about the development of this bot.")
+    fields << Discord::EmbedField.new(name: "+leave",   value: "Removes you from the queue.")
+    fields << Discord::EmbedField.new(name: "+queue",   value: "Shows all the people currently looking for partners.")
+    fields << Discord::EmbedField.new(name: "+matches", value: "Shows you everyone in the queue who can potentially play with you.")
+    fields << Discord::EmbedField.new(name: "+info",    value: "Displays some info about the development of this bot.")
 
     embed.fields = fields
     embed.colour = 0x733430
@@ -35,6 +35,6 @@ class GodsMatchmaking::Help
   )]
   def set_game(payload)
     # For some reason I need to send this 0, otherwise Discord refuses to update the game.
-    client.status_update(game: Discord::GamePlaying.new("God's Trigger | -help", 0.to_i64))
+    client.status_update(game: Discord::GamePlaying.new("God's Trigger | +help", 0.to_i64))
   end
 end
